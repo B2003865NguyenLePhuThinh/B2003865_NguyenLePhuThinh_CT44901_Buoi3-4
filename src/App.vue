8ea07ee3d51e0559ccd8d687098f96fea6b08503
ThinhNguyen <thinhbo2999@gmail.com>
2022-10-23 21:11:36 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
+import AppHeader from "@/compoments/AppHeader.vue"
 export default {
-  
+	components:{
+		AppHeader,
+	}
 }
 </script>
 
 <template>
-  <h1>Hello, Vue.js! </h1>	
+	<div id="app">
+		<AppHeader/>
+		<div class="conptainer mt-3">
+			<router-view/>
+		</div>
+	</div>
 </template>
 
 <style>
